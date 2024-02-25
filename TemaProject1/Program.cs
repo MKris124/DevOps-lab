8c44e02b5fa9ab751a1db3edb45cf28aef85c5d4
MKris124 <kristofmora124@gmail.com>
2024-02-25 21:05:29 +0100

    again

---
+++ TemaProject1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaProject1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string sampleText = "Hello World";
+            string firstTrimmed = TrimFirstCharacter(sampleText);
+            string lastTrimmed = TrimLastCharacter(firstTrimmed);
+
+            Console.WriteLine($"Original: {sampleText}");
+            Console.WriteLine($"First Character Trimmed: {firstTrimmed}");
+            Console.WriteLine($"Last Character Trimmed: {lastTrimmed}");
+            Console.ReadKey();
+        }
+
+        public static string TrimFirstCharacter(string input)
+        {
+            return string.Join(" ", input.Split(' ').Select(word => word.Substring(1)));
+        }
+
+        public static string TrimLastCharacter(string input)
+        {
+            return string.Join(" ", input.Split(' ').Select(word => word.Substring(0, word.Length - 1)));
+        }
+    }
+}
--- TestProject/TestString.cs
+++ TestProject/TestString.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TemaProject;
+using TemaProject1;
 
 namespace TestProject
 {
